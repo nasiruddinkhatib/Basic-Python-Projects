3067f44c702c959289498502d27fc6c4b433540a
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-13 00:17:21 +0530

    Rock_Paper_Scissor.py

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -21,10 +21,34 @@ Scissor - Rock = Rock win
 Scissor - Paper = Scissor win
 
 """
+"""
+WORKFLOW OF PROJECT:
+1- Input from user(Rock, paper, scissor)
+2- Computer choice (Computer will choose randomly not conditionally)
+3- Result print
+
+Cases:
+A- Rock
+Rock - Rock = tie
+Rock - Paper = Paper win
+Rock - scissor = Rock win
+
+B- Paper
+Paper - Paper = tie
+Paper - Rock = Paper win
+Paper - Scissor = Scissor win
+
+C- Scissor
+Scissor - Scissor = tie
+Scissor - Rock = Rock win
+Scissor - Paper = Scissor win
+
+"""
+
 import random
 item_list = ["Rock", "Paper", "Scissor"]
 
-user_choice = input("Enter your move = Rock, Paper, Scissor= ")
+user_choice = input("Enter your move = Rock, Paper, Scissor= ").strip().upper()
 comp_choice = random.choice(item_list)
 
 print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
@@ -32,19 +56,19 @@ print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
 if user_choice == comp_choice:
     print("Both chooses same: = Match Tie")
 
-elif user_choice == "Rock":
+elif user_choice == "Rock" or'r':
     if comp_choice == "Paper":
         print("Paper covers Rock = Computer Win")
     else:
         print("Rock smashes Scissor = You win")
 
-elif user_choice == "Paper":
+elif user_choice == "Paper" or 'p':
     if comp_choice == "Scissor":
         print("Scissor cuts paper, Computer Win")
     else:
         print("Paper covers rock, You win")
 
-elif user_choice == "Scissor":
+elif user_choice == "Scissor" or 's':
     if comp_choice == "Paper":
         print("Scissor cuts paper, You win")
     else:
